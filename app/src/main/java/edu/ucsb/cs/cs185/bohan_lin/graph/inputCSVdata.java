0060a4df679d47921f1d8d8ae1502106f0159b5c
mohit.hingorani <mohit.hingorani@umail.ucsb.edu>
2015-06-06 01:51:00 -0700

    adding csv data class

---
+++ app/src/main/java/edu/ucsb/cs/cs185/bohan_lin/graph/inputCSVdata.java
@@ -0,0 +1,31 @@
+package edu.ucsb.cs.cs185.bohan_lin.graph;
+
+/**
+ * Created by mohithingorani on 6/6/15.
+ */
+public class inputCSVdata {
+    String name;
+
+    //ArrayList<Float> floatData;
+
+    /*
+                            Float economy;
+                            Float cylinders;
+                            Float displacement;
+                            Float power;
+                            Float weight;
+                            Float accelerate;
+                            Float year;
+*/
+
+
+    Float []dataFloats ;
+
+    public inputCSVdata()
+    {
+        name = null;
+        dataFloats = new Float[7];
+
+    }
+
+}
